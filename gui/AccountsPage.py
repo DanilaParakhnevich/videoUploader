5c41e116f6e75734aa4771da55b8fb633207c54e
DanilaParakhnevich <beylon228822@gmail.com>
2023-09-15 00:56:11 +0300

    fixed

--- gui/AccountsPage.py
+++ gui/AccountsPage.py
@@ -1,4 +1,5 @@
 import traceback
+from functools import partial
 
 from PyQt5 import QtCore, QtWidgets
 
@@ -24,7 +25,7 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
         super(AccountsPageWidget, self).__init__(central_widget)
         self.setMinimumSize(QtCore.QSize(0, 440))
         self.setObjectName("accounts_page_widget")
-        self.setColumnCount(3)
+        self.setColumnCount(4)
         self.setRowCount(0)
         item = QtWidgets.QTableWidgetItem()
         self.setHorizontalHeaderItem(0, item)
@@ -32,6 +33,8 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
         self.setHorizontalHeaderItem(1, item)
         item = QtWidgets.QTableWidgetItem()
         self.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(3, item)
         self.horizontalHeader().setDefaultSectionSize(310)
 
         horizontal_layout = QtWidgets.QHBoxLayout(self)
@@ -56,6 +59,8 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
         item = self.horizontalHeaderItem(1)
         item.setText(get_str('channel'))
         item = self.horizontalHeaderItem(2)
+        item.setText(get_str('reauthorize'))
+        item = self.horizontalHeaderItem(3)
         item.setText(get_str('delete'))
         self.add_button.setText(get_str('add'))
 
@@ -67,9 +72,14 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
             input_position = self.rowCount() - 1
 
             btn = QtWidgets.QPushButton(self)
+            btn.setText(get_str('reauthorize'))
+            btn.clicked.connect(partial(self.reauthorize, account.url if hasattr(account, 'url') and account.url is not None else account.login, account.hosting))
+            self.setCellWidget(input_position, 2, btn)
+
+            btn = QtWidgets.QPushButton(self)
             btn.setText('-')
             btn.clicked.connect(self.on_delete_row)
-            self.setCellWidget(input_position, 2, btn)
+            self.setCellWidget(input_position, 3, btn)
 
             self.setItem(input_position, 0, item1)
             self.setItem(input_position, 1, item2)
@@ -127,8 +137,13 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
             input_position = self.rowCount() - 1
 
             btn = QtWidgets.QPushButton(self)
-            btn.setText('-')
+            btn.setText(get_str('reauthorize'))
+            btn.clicked.connect(partial(self.reauthorize, account.url if hasattr(account, 'url') and account.url is not None else account.login))
             self.setCellWidget(input_position, 2, btn)
+
+            btn = QtWidgets.QPushButton(self)
+            btn.setText('-')
+            self.setCellWidget(input_position, 3, btn)
 
             btn.clicked.connect(self.on_delete_row)
 
@@ -142,6 +157,51 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
             self.state_service.save_accounts(current_accounts)
 
         self.add_button.stop_animation()
+
+    def reauthorize(self, name, hosting):
+        by_login = self.state_service.get_account_by_hosting_and_login(hosting, name)
+        by_url = self.state_service.get_account_by_hosting_and_url(hosting, name)
+        account = by_url if by_url is not None else by_login
+        hosting = Hosting[hosting]
+
+        account = hosting.value[0].show_login_dialog(hosting, form=self, title='check_fail',
+                                                     login=account.login,
+                                                     password=account.password if self.state_service.get_settings().save_password else '',
+                                                     can_relogin=True)
+
+        if account is None:
+            return
+
+        index = 0
+        for acc in self.state_service.get_accounts():
+            if acc.hosting == account.hosting and acc.login == account.login and acc.url == account.url:
+                break
+            index += 1
+
+        msg = QtWidgets.QMessageBox(self)
+        msg.setText(get_str('authorized_successfully'))
+
+        if hosting.value[0].need_to_pass_channel_after_login():
+            try:
+                if hosting.value[0].validate_url_by_account(account.destination, account) is False:
+                    msg = QtWidgets.QMessageBox(self)
+                    msg.setText(f'{get_str("failed_account_validation")}: {account.destination}')
+                    self.event_service.add_event(Event(f'{get_str("failed_account_validation")}: {account.destination}'))
+                    msg.exec_()
+                    return
+            except:
+                msg.exec_()
+                log_error(traceback.format_exc())
+                self.event_service.add_event(Event(f'{get_str("failed_account_validation")}: {account.url}'))
+                self.add_button.stop_animation()
+                return
+        msg.exec_()
+
+        button = self.sender()
+        row = self.indexAt(button.pos()).row()
+        self.item(row, 1).setText(account.url if account.url is not None else account.login)
+        self.accounts[row] = account
+        self.state_service.save_accounts(self.accounts)
 
     def on_delete_row(self):
         button = self.sender()
@@ -164,8 +224,8 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
         self.change = False
         coef_x = self.parent().width() / 950
 
-        if self.state_service.column_row('accounts', 0) is None or self.state_service.column_row('accounts', 1) is None or self.state_service.column_row('accounts', 2) is None:
-            column_width = int(950 / 3)
+        if self.state_service.column_row('accounts', 0) is None or self.state_service.column_row('accounts', 1) is None or self.state_service.column_row('accounts', 2) is None or self.state_service.column_row('accounts', 3) is None:
+            column_width = int(950 / 4)
 
             if self.state_service.column_row('accounts', 0) is None:
                 self.state_service.save_column_row('accounts', 0, column_width)
@@ -173,14 +233,18 @@ class AccountsPageWidget(QtWidgets.QTableWidget):
                 self.state_service.save_column_row('accounts', 1, column_width)
             if self.state_service.column_row('accounts', 2) is None:
                 self.state_service.save_column_row('accounts', 2, column_width)
+            if self.state_service.column_row('accounts', 3) is None:
+                self.state_service.save_column_row('accounts', 3, column_width)
 
         width_0 = int(self.state_service.column_row('accounts', 0) * coef_x)
         width_1 = int(self.state_service.column_row('accounts', 1) * coef_x)
         width_2 = int(self.state_service.column_row('accounts', 2) * coef_x)
+        width_3 = int(self.state_service.column_row('accounts', 3) * coef_x)
 
         self.setColumnWidth(0, width_0)
         self.setColumnWidth(1, width_1)
         self.setColumnWidth(2, width_2)
+        self.setColumnWidth(3, width_3)
 
         self.change = True
 
